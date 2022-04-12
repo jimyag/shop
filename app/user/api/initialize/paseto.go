17b18fa4513d205949f086af22c3ab370db93633
Jimyag <jimyag@126.com>
2022-04-12 20:32:05 +0800

    fix(user-api): 修复token认证失败

--- app/user/api/initialize/paseto.go
+++ app/user/api/initialize/paseto.go
@@ -22,12 +22,15 @@ func InitPaseto() {
 	}
 	privateKey := ed25519.PrivateKey(b)
 
-	b, err = hex.DecodeString(global.RemoteConfig.Secret.PrivateKey)
+	b, err = hex.DecodeString(global.RemoteConfig.Secret.PublicKey)
 	if err != nil {
 		global.Logger.Error("初始化公钥错误", zap.Error(err))
 	}
 	publicKey := ed25519.PublicKey(b)
 
-	global.PasetoMaker, _ = paseto.NewPasetoMaker(privateKey, publicKey, time.Duration(global.RemoteConfig.Secret.Duration))
+	global.PasetoMaker, err = paseto.NewPasetoMaker(privateKey, publicKey, time.Duration(global.RemoteConfig.Secret.Duration))
+	if err != nil {
+		global.Logger.Fatal("初始化PASETO失败", zap.Error(err))
+	}
 	global.Logger.Info("初始化PASETO成功......")
 }
