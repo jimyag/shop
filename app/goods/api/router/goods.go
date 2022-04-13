7aaf1587924c5e931ffae3bce1b2328900e80f46
Jimyag <jimyag@126.com>
2022-04-13 20:06:49 +0800

    feat(goods-api): 增加路由url

--- app/goods/api/router/goods.go
+++ app/goods/api/router/goods.go
@@ -3,21 +3,25 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/jimyag/shop/app/goods/api/api"
 	"github.com/jimyag/shop/app/goods/api/middlewares"
 )
 
 func GoodsRouter(router *gin.RouterGroup) {
 	baseRouter := router.Group("")
 	baseRouter.Use(middlewares.Tracing())
-	publicRouter := baseRouter.Group("user")
+	publicRouter := baseRouter.Group("goods")
 	publicRouter.Use()
 	{
-
+		publicRouter.GET("info", api.GetGoods)
+		publicRouter.GET("infos", api.GetBatchGoods)
 	}
 
-	privateRouter := baseRouter.Group("user")
+	privateRouter := baseRouter.Group("goods")
 	privateRouter.Use(middlewares.Paseto())
 	{
-
+		privateRouter.POST("create", api.CreateGoods)
+		privateRouter.PUT("info", api.UpdateGoodsInfo)
+		privateRouter.DELETE("info", api.DeleteGoods)
 	}
 }
