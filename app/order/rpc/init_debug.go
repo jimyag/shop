b1963de37c1da129ef601b5841986ef8543d971e
Jimyag <jimyag@126.com>
2022-04-14 15:36:21 +0800

    feat(order-rpc): 同步本地配置文件到配置中心

---
+++ app/order/rpc/init_debug.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/jimyag/shop/common/utils/initialize"
+
+const (
+	consulAddress = "http://localhost:8500"
+	debugPath     = "shop/order/rpc/debug.yaml"
+	releasePath   = "shop/order/rpc/release.yaml"
+	fileName      = "remote-config.yaml"
+)
+
+//
+// main
+//  @Description: 用来初始化远程配置文件
+//
+func main() {
+	initialize.LocalConfigToRemoteConfig(consulAddress, debugPath, releasePath, fileName)
+}
