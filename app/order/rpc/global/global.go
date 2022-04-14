663ef7e28a16d26cddd52aa964bb0666d06a8317
Jimyag <jimyag@126.com>
2022-04-14 15:36:39 +0800

    feat(order-rpc): 全局变量

---
+++ app/order/rpc/global/global.go
@@ -0,0 +1,17 @@
+package global
+
+import (
+	"database/sql"
+
+	"go.uber.org/zap"
+
+	remoteConfig "github.com/jimyag/shop/app/order/rpc/config"
+	"github.com/jimyag/shop/common/model"
+)
+
+var (
+	Logger       *zap.Logger             // logger
+	RemoteConfig *remoteConfig.ALLConfig //远程配置中心里面的配置
+	ConfigCenter *model.ConfigCenterInfo //配置中心的位置信息
+	DB           *sql.DB                 // database
+)
