d2d2e5a9dae252e7f0e1a8462da2d5447d668e19
Jimyag <jimyag@126.com>
2022-04-15 22:13:00 +0800

    feat(order-api): 初始化路由

--- app/order/api/initialize/router.go
+++ app/order/api/initialize/router.go
@@ -3,6 +3,7 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 
+	router2 "github.com/jimyag/shop/app/order/api/router"
 	healthRouter "github.com/jimyag/shop/common/utils/router"
 )
 
@@ -19,6 +20,8 @@ func InitRouter() *gin.Engine {
 	healthRouter.HealthRouter(rootGroup)
 
 	// order 的路由
-	_ = router.Group("/order/v1")
+	orderRouter := router.Group("/order/v1")
+	router2.OrderRouter(orderRouter)
+	router2.ShopCartRouter(orderRouter)
 	return router
 }
