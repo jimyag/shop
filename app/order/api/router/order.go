4fedf2fee48110089bb96745146b1bf0f65aedb6
Jimyag <jimyag@126.com>
2022-04-15 22:12:27 +0800

    feat(order-api): 初始化路由

--- app/order/api/router/order.go
+++ app/order/api/router/order.go
@@ -3,21 +3,21 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/jimyag/shop/app/order/api/api"
 	"github.com/jimyag/shop/app/order/api/middlewares"
 )
 
-func orderRouter(router *gin.RouterGroup) {
+func OrderRouter(router *gin.RouterGroup) {
 	baseRouter := router.Group("")
 	baseRouter.Use(middlewares.Tracing())
 	publicRouter := baseRouter.Group("order")
 	publicRouter.Use()
-	{
-
-	}
 
 	privateRouter := baseRouter.Group("order")
 	privateRouter.Use(middlewares.Paseto())
 	{
-
+		privateRouter.POST("create", api.CreateOrder) // 创建订单
+		privateRouter.GET("info", api.GetOrderDetail) // 获得订单详情
+		privateRouter.GET("infos", api.GetOrderList)  // 获得个人订单列表
 	}
 }
--- app/order/api/router/shop_cart.go
+++ app/order/api/router/shop_cart.go
@@ -1,1 +1,24 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/jimyag/shop/app/order/api/api"
+	"github.com/jimyag/shop/app/order/api/middlewares"
+)
+
+func ShopCartRouter(router *gin.RouterGroup) {
+	baseRouter := router.Group("")
+	baseRouter.Use(middlewares.Tracing())
+	publicRouter := baseRouter.Group("cart")
+	publicRouter.Use()
+
+	privateRouter := baseRouter.Group("cart")
+	privateRouter.Use(middlewares.Paseto())
+	{
+		privateRouter.POST("create", api.CreateShopCart)       // 添加商品到购物车记录
+		privateRouter.GET("list", api.GetShopCartList)         // 获得购物车列表
+		privateRouter.DELETE("remove", api.DeleteShopCartItem) // 删除购物车的某条记录
+		privateRouter.PUT("update", api.UpdateShopCartItem)    // 更新的某条记录
+	}
+}
