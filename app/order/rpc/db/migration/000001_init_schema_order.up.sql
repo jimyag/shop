a6124f9ce3f9dc32623d3c7f0d5a9aa03d2e6d32
Jimyag <jimyag@126.com>
2022-04-15 17:41:06 +0800

    refactor(order-rpc): 重构订单编号为int64

--- app/order/rpc/db/migration/000001_init_schema_order.up.sql
+++ app/order/rpc/db/migration/000001_init_schema_order.up.sql
@@ -13,20 +13,20 @@ CREATE TABLE "shopping_cart"
 CREATE TABLE "order_info"
 (
     "id"            bigserial PRIMARY KEY,
-    "created_at"    timestamptz    NOT NULL DEFAULT (now()),
-    "updated_at"    timestamptz    NOT NULL DEFAULT (now()),
-    "deleted_at"    timestamptz             DEFAULT null,
-    "user_id"       integer        NOT NULL,
-    "order_id"      integer UNIQUE NOT NULL,
+    "created_at"    timestamptz NOT NULL DEFAULT (now()),
+    "updated_at"    timestamptz NOT NULL DEFAULT (now()),
+    "deleted_at"    timestamptz          DEFAULT null,
+    "user_id"       integer     NOT NULL,
+    "order_id"      int8 UNIQUE NOT NULL,
     "pay_type"      varchar,
-    "status"        int8           NOT NULL, -- 1 待支付 2 成功 3 超时关闭
-    "trade_id"      varchar, --支付编号
-    "order_mount"   float,   -- 订单金额
+    "status"        int2        NOT NULL, -- 1 待支付 2 成功 3 超时关闭
+    "trade_id"      varchar,              --支付编号
+    "order_mount"   float,                -- 订单金额
     "pay_time"      timestamptz,
-    "address"       varchar        NOT NULL,
-    "signer_name"   varchar(40)    NOT NULL,
-    "signer_mobile" varchar(20)    NOT NULL,
-    "post"          varchar        NOT NULL
+    "address"       varchar     NOT NULL,
+    "signer_name"   varchar(40) NOT NULL,
+    "signer_mobile" varchar(20) NOT NULL,
+    "post"          varchar     NOT NULL
 );
 
 CREATE TABLE "order_goods"
@@ -35,7 +35,7 @@ CREATE TABLE "order_goods"
     "created_at"  timestamptz NOT NULL DEFAULT (now()),
     "updated_at"  timestamptz NOT NULL DEFAULT (now()),
     "deleted_at"  timestamptz          DEFAULT null,
-    "order_id"    integer     NOT NULL,
+    "order_id"    int8        NOT NULL,
     "goods_id"    integer     NOT NULL,
     "goods_name"  varchar     NOT NULL,
     "goods_price" float       NOT NULL,
