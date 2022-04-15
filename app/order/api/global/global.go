2a4b30ed94f62fb915afbc6291d338842b694653
Jimyag <jimyag@126.com>
2022-04-15 22:12:08 +0800

    feat(order-api): 添加商品服务

--- app/order/api/global/global.go
+++ app/order/api/global/global.go
@@ -16,6 +16,7 @@ var (
 	ConfigCenter   *model.ConfigCenterInfo // 配置中心的配置
 	RemoteConfig   *config.ServerInfo      // 配置中心中的配置
 	OrderSrvClient proto.OrderClient       // order grpc 服务的客户端
+	GoodsSrvClient proto.GoodsClient       // order grpc 服务的客户端
 	Trans          ut.Translator           // 公共的翻译
 	Validate       *validator.Validate     // 公共的validate
 	PasetoMaker    *paseto.PasetoMaker     // paseto 的maker
