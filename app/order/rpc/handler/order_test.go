b3f05134212793d116747b16a3f9cebca9dc109d
Jimyag <jimyag@126.com>
2022-04-15 17:44:33 +0800

    test(order-rpc): 增加创建订单和查看订单信息的tc

--- app/order/rpc/handler/order_test.go
+++ app/order/rpc/handler/order_test.go
@@ -31,6 +31,10 @@ func TestGetCartList(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, len(list.Data) > 0)
 	t.Log(list)
+
+	list, err = orderClient.CartItemList(context.Background(), &proto.CartItemListRequest{Uid: 117})
+	require.Error(t, err)
+	require.Nil(t, list)
 }
 
 func TestOrderServer_UpdateCartItem(t *testing.T) {
@@ -49,4 +53,27 @@ func TestOrderServer_UpdateCartItem(t *testing.T) {
 		Checked: false,
 	})
 	require.Error(t, err)
+}
+
+func TestOrderServer_CreateOrder(t *testing.T) {
+	order, err := orderClient.CreateOrder(context.Background(), &proto.CreateOrderRequest{
+		UserID:  116,
+		Address: "中国上海",
+		Mobile:  "18522222222",
+		Name:    "jimyag",
+		Post:    "201314",
+	})
+	require.NoError(t, err)
+	require.NotNil(t, order)
+	t.Logf("%v", order)
+}
+
+func TestOrderServer_GetOrderDetail(t *testing.T) {
+	rsp, err := orderClient.GetOrderDetail(context.Background(),
+		&proto.GetOrderDetailRequest{
+			OrderID: 20224151781711695,
+		})
+	require.NoError(t, err)
+	require.NotEmpty(t, rsp)
+	t.Logf("%v", rsp)
 }
